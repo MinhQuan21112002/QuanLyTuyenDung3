ce6d811b09bcf223b5f7f72f9672a796df1d1d07
Nguyen Khanh <87373594+nguyenkhanh2kpi@users.noreply.github.com>
2023-12-22 14:48:52 +0700

    final-chart

--- src/Service/user.servie.js
+++ src/Service/user.servie.js
@@ -43,7 +43,29 @@ const removeBlacklist = async (token, id) => {
   }
 }
 
+
+const getAllUser = async (token) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }
+    const res = await axios.get(`${API_URL}/user`,config)
+    return res.data
+  } catch (error) {
+    const axiosError = error
+    if (axiosError && axiosError.response && axiosError.response.status === 403) {
+      throw new Error('no_permistion')
+    } else {
+      throw error
+    }
+  }
+}
+
 export const userService = {
   addBlacklist,
   removeBlacklist,
+  getAllUser,
 }
